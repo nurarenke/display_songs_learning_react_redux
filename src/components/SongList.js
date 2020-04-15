89e04967719a8e13337b03803d757ca7cffbabda
Nura Renke <nurarenke@Nuras-MacBook-Pro.local>
2020-04-15 15:16:00 -0700

    Add SongList Component

---
+++ src/components/SongList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"; //using name export if you want
+
+// you can use just Component instead of React.Component
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+export default SongList;
