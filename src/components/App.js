3088dc8b79c5bb0971f1c1cbecedec44e1d17c7d
Nura Renke <nurarenke@Nuras-MacBook-Pro.local>
2020-04-20 10:21:35 -0700

    Add Song Detail Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    //using semantic ui's grid system
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react"; //using name export if you want
+import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 // you can use just Component instead of React.Component
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
-export default SongList;
+// get specific data out our redux store
+const mapStateToProps = (state) => {
+  //return as props in our SongList component
+  return { songs: state.songs };
+};
+
+// connect is from redux
+export default connect(mapStateToProps, {
+  selectSong, //aka selectSong: selectSong thru es2015 syntax
+})(SongList);
